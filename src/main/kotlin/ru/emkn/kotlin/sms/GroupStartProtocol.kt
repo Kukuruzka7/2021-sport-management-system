1adda1a34fb77d802e6138cc4421e6d72ed9dd8d
TEAmofey <teamofey1982@gmail.com>
2021-11-23 11:58:17 +0300

    Moved classes into one file

--- src/main/kotlin/ru/emkn/kotlin/sms/GroupStartProtocol.kt
+++
@@ -1,7 +0,0 @@
-package ru.emkn.kotlin.sms
-
-import java.io.File
-
-class GroupStartProtocol(val group: Group) {
-    val toCsv: File = TODO()
-}
--- src/main/kotlin/ru/emkn/kotlin/sms/StartProtocol.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/StartProtocol.kt
@@ -1,8 +1,14 @@
 package ru.emkn.kotlin.sms
 
+import java.io.File
+
 class StartProtocol(listGroup: List<Group>) {
     val generateCsv: List<GroupStartProtocol>
     init {
         generateCsv = listGroup.map { GroupStartProtocol(it) }
     }
+}
+
+class GroupStartProtocol(val group: Group) {
+    val toCsv: File = TODO()
 }
