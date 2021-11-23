73fec2bd1aa1c9793a74cfab62e93eaaec486d74
ksenono <danil.sibgatulllin@gmail.com>
2021-11-23 22:33:59 +0300

    InputCompetitionResult.kt & minor bugs fixed

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/FinishProtocol.kt
@@ -0,0 +1,28 @@
+package ru.emkn.kotlin.sms
+
+class TeamProtocol() {
+    fun toCSV(): Any = TODO()
+}
+
+class GroupProtocol {
+    fun toCSV(): Any = TODO()
+}
+
+class AthleteProtocol(fileName: String) {
+
+}
+
+class Protocol(table: Table) {
+    private val groupProtocol: Map<Group, GroupProtocol> = TODO()
+    private val teamProtocols: Map<Team, TeamProtocol> = TODO()
+    private val athleteProtocol: Map<Athlete, AthleteProtocol> = TODO()
+
+    val csvByTeams = generateCSVbyTeams()
+    val csvByGroups = generateCSVbyGroups()
+    val overallCSB = generateOverallCSV()
+
+    private fun generateCSVbyTeams(): Any = TODO()
+    private fun generateCSVbyGroups(): Any = TODO()
+    private fun generateOverallCSV(): Any = TODO()
+
+}
