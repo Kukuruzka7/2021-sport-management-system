b9f6355ab9108ebf90685f0fc59c580a343d1ce5
TEAmofey <teamofey1982@gmail.com>
2021-11-24 14:07:00 +0300

    Разделил Старт протокол и делаю в более ООПшном стиле

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,36 +1,38 @@
 package ru.emkn.kotlin.sms
 
+import kotlinx.datetime.LocalDate
 import ru.emkn.kotlin.sms.startprotocol.GroupStartProtocol
 import ru.emkn.kotlin.sms.startprotocol.StartProtocol
 import java.util.*
 
 fun main(args: Array<String>) {
-    var birthDate = GregorianCalendar()
-
-    birthDate.set(2003, 10, 11, 0, 0, 0)
 
     val athlete1 = Athlete(
         Name("Миргалимова", "Розалина"),
         Sex.FEMALE,
-        null,
+        LocalDate.parse("2003-01-01"),
         Category("I"),
-        GroupType("abobus")
+        GroupType("Amogus"),
+        Team(TeamName("aboba"))
     )
-    birthDate.set(2002, 12, 31, 0, 0, 0);
+
     val athlete2 = Athlete(
         Name("Сибгатуллин", "Данил"),
         Sex.MALE,
-        null,
+        LocalDate.parse("2002-01-01"),
         Category("КМС"),
-        GroupType("aboba")
+        GroupType("abobus"),
+        Team(TeamName("aboba"))
     )
-    birthDate.set(2004, 3, 24, 0, 0, 0);
+
     val athlete3 = Athlete(
         Name("Москаленко", "Тимофей"),
         Sex.MALE,
-        null,
+        LocalDate.parse("2002-01-01"),
         Category("III"),
-        GroupType("aboba")
+        GroupType("abobus"),
+        Team(TeamName("aboba")),
+
     )
     val group1 = Group(GroupType("AMOGUS"), listOf(athlete1, athlete2, athlete3, athlete1))
     val group2 = Group(GroupType("Danil"), listOf(athlete2, athlete2, athlete2))
