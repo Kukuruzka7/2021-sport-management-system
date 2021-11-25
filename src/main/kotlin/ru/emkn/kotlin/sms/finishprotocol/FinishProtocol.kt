8126eb8205f9b2d81d51d4a5283cce65a04a2933
TEAmofey <teamofey1982@gmail.com>
2021-11-26 01:10:21 +0300

    Сделал класс AthleteResult. Добавил реализацию FinishProtocol. Реализовал там почти все протоколы. Добавил оператор - для LocalDateTime.

--- src/main/kotlin/ru/emkn/kotlin/sms/finishprotocol/FinishProtocol.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/finishprotocol/FinishProtocol.kt
@@ -5,27 +5,40 @@ import ru.emkn.kotlin.sms.athlete.Category
 import java.time.LocalDateTime
 import java.time.Year
 
-class TeamProtocol {
+class TeamProtocol(val team: Team, map: Map<Athlete, AthleteProtocol?>) {
+    val protocol = map.filter { team.athletes.contains(it.key) }
     val toCSV: Any = TODO()
 }
 
-class GroupProtocol {
+class GroupProtocol(val group: Group, map: Map<Athlete, AthleteProtocol?>) {
+    val protocol = map.filter { group.athletes.contains(it.key) }
     val toCSV: Any = TODO()
 }
 
-data class AthleteProtocol(
-    val place: Int,
+data class AthleteResult(
     val number: AthleteNumber,
     val name: Name,
-    val year: Year,
+    val year: Int,
     val category: Category,
-    val finishTime: LocalDateTime
+    val finishTime: LocalDateTime?,
 )
 
-class FinishProtocol(table: Table, competition: Competition) {
-    private val groupProtocol: Map<Group, GroupProtocol?> = TODO()
+data class AthleteProtocol(
+    val result: AthleteResult?,
+    val place: Int
+)
+
+class FinishProtocol(private val table: Table, competition: Competition) {
+
+    companion object {
+        const val dir = "src/main/resources/competitions/"
+    }
+
+    private val athleteResult: Map<Athlete, AthleteResult?> =
+        competition.athleteList.associateWith { makeIndividualResults(it) }
+    private val athleteProtocol: Map<Athlete, AthleteProtocol?> = makeSortedResults(athleteResult)
+    private val groupProtocol: Map<Group, GroupProtocol?> = toGroupProtocol(athleteProtocol)
     private val teamProtocols: Map<Team, TeamProtocol?> = TODO()
-    private val athleteProtocol: Map<Athlete, AthleteProtocol?> = TODO()
 
     val csvByTeams = generateCSVbyTeams()
     val csvByGroups = generateCSVbyGroups()
@@ -35,14 +48,39 @@ class FinishProtocol(table: Table, competition: Competition) {
     private fun generateCSVbyGroups(): Any = TODO()
     private fun generateOverallCSV(): Any = TODO()
 
-    init {
-        athleteProtocol = competition.athleteList.associateWith { makeIndividualProtocol(it, table[it.number]) }
+    private fun makeIndividualResults(athlete: Athlete): AthleteResult {
+        //Время начала и конца путешествия одного чела
+        val startTime = table.startTime[athlete.number]
+        val finishTime = table[athlete.number]?.get(FinishCheckPoint)?.date
+        //Очень сильно просим, чтобы чел начал дистанцию
+        require(startTime != null) { "Нет стартового времени у чела под номером ${athlete.number}" }
+        return AthleteResult(
+            athlete.number,
+            athlete.name,
+            athlete.birthDate.year,
+            athlete.sportCategory,
+            finishTime - startTime,
+        )
     }
 
-    private fun makeIndividualProtocol (athlete: Athlete, map: Map<CheckPoint, CheckPointRes>?): AthleteProtocol? {
-        if(map == null) {
-            return null
-        }
-        return null
+    private fun makeSortedResults(athleteResult: Map<Athlete, AthleteResult?>): Map<Athlete, AthleteProtocol?> {
+        val sortedList = athleteResult.toList().sortedBy { it.second?.finishTime }
+        return sortedList.associate { it.first to AthleteProtocol(it.second, sortedList.indexOf(it) + 1) }
     }
+
+    private fun toGroupProtocol(athleteProtocol: Map<Athlete, AthleteProtocol?>): Map<Group, GroupProtocol?> {
+        val groups = Competition.generateGroupListByAthleteList(athleteProtocol.keys.toList())
+        return groups.associateWith { GroupProtocol(it, athleteProtocol) }
+    }
+
+    private fun toTeamProtocol(athleteProtocol: Map<Athlete, AthleteProtocol?>): Map<Team, TeamProtocol?> {
+        val teams = Competition.generateTeamListByAthleteList(athleteProtocol.keys.toList())
+        return teams.associateWith { TeamProtocol(it, athleteProtocol) }
+    }
+}
+
+//Функция разности двух LocalDateTime
+operator fun LocalDateTime?.minus(start: LocalDateTime): LocalDateTime? {
+    return this?.minusHours(start.hour.toLong())?.minusMinutes(start.minute.toLong())
+        ?.minusSeconds(start.second.toLong())
 }
