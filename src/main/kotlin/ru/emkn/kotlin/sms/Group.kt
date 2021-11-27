795e857352ec43d89878f21a9eeb239a49a7d490
Kukuruzka7 <yarozalina@gmail.com>
2021-11-27 15:26:23 +0300

    Added processing race.

--- src/main/kotlin/ru/emkn/kotlin/sms/Group.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Group.kt
@@ -2,6 +2,7 @@ package ru.emkn.kotlin.sms
 
 import java.io.File
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import ru.emkn.kotlin.sms.result_data.Checkpoint
 
 class GroupName(val groupName: String) {
     override fun toString() = groupName
@@ -11,12 +12,12 @@ class Race(val groupName: GroupName, sportType: SportType) {
     val checkPoints = getCheckPoints(groupName.groupName, sportType)
     override fun toString() = groupName.toString()
 
-    private fun getCheckPoints(groupName: String, sportType: SportType): List<CheckPoint> {
+    private fun getCheckPoints(groupName: String, sportType: SportType): List<Checkpoint> {
         val course = classesBySportType[sportType]?.find { it[0] == groupName }?.get(1)
             ?: throw WeHaveAProblem("Здесь не должно быть null")
         val courseList = coursesBySportType[sportType]?.find { it[0] == course }
             ?: throw WeHaveAProblem("Здесь не должно быть null")
-        return courseList.subList(1, courseList.lastIndex).map { CheckPoint(it) }
+        return courseList.subList(1, courseList.lastIndex).map { Checkpoint(it) }
     }
 
     companion object {
