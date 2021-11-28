c7b8e31c8f6f35f59ae10133c2f82fc6ac5fcf3a
TEAmofey <teamofey1982@gmail.com>
2021-11-28 19:09:54 +0300

    Сделан первый тест

--- src/test/kotlin/StartProtocolTests.kt
+++ src/test/kotlin/StartProtocolTests.kt
@@ -1,9 +1,35 @@
-import kotlin.test.*
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import kotlinx.datetime.LocalDate
+import ru.emkn.kotlin.sms.*
+import ru.emkn.kotlin.sms.athlete.*
+import ru.emkn.kotlin.sms.startprotocol.GroupStartProtocol
+import java.io.File
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class StartProtocolTests {
 
     @Test
-    fun test1() {
-        assert(true)
+    fun testNumberAndStartTime() {
+        sport = SportType.RUNNING
+        val num = Random.nextInt(1, 59)
+        val athlete = Athlete(
+            Name("Данил Сибгатуллин"),
+            Sex.MALE,
+            LocalDate(2002, 12, 30),
+            Category.I,
+            GroupName("М2002"),
+            TeamName("Б05"),
+            GroupName("М2002"),
+            AthleteNumber("$num")
+        )
+        val group = Group(Race(GroupName("М2002")), listOf(athlete))
+
+        GroupStartProtocol(group, "src/test/resources/StartProtocolTests/")
+        val list = csvReader().readAll(File("src/test/resources/StartProtocolTests/М2002.csv"))
+        assertEquals(num.toString(), list[1][0])
+        assertEquals("12:${num / 10}${num % 10}:00", list[1][5])
+        File("src/test/resources/StartProtocolTests/М2002.csv").delete()
     }
 }
