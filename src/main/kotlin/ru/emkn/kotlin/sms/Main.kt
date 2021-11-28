903771e3512500179420dc0dfa22551f43b0758a
Kukuruzka7 <yarozalina@gmail.com>
2021-11-28 18:29:46 +0300

    main refactor

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -55,41 +55,15 @@ fun main(args: Array<String>) {
 }
 
 fun start(inputData: Array<String>) {
-    if (inputData.size != FieldsStart.values().size) {
-        println("Вы ввели что-то не то. Попробуйте еще раз.")
-        return
-    }
+    if (checkForEmptyInput(inputData)) return
     val name = inputData[FieldsStart.NAME.ordinal]
     sport = getSportType(inputData[FieldsStart.SPORT_TYPE.ordinal])
     val dateString = inputData[FieldsStart.DATE.ordinal]
     val fileName = inputData[FieldsStart.FILE_NAME_OF_APPLICATION.ordinal]
-    if (sport == SportType.ERROR) {
-        println("Спорт ${inputData[FieldsStart.SPORT_TYPE.ordinal]} наша система не поддерживает.")
-        return
-    }
-    val date: LocalDate
-    try {
-        date = dateString.toLocalDate()
-    } catch (e: Exception) {
-        println("Дата состязания $dateString не корректна.")
-        return
-    }
-    val application: Application
-    val teamApplicationNames: List<String>
-    try {
-        teamApplicationNames = File(fileName).readLines()
-    } catch (e: Exception) {
-        println("Файл $fileName не может быть прочитан.")
-        return
-    }
-    try {
-        application = Application(teamApplicationNames.map {
-            File(it)
-        })
-    } catch (e: Exception) {
-        println(e.message)
-        return
-    }
+    if (checkSportType(inputData)) return
+    val date: LocalDate = getDate(dateString) ?: return
+    val teamApplicationNames: List<String> = getTeamApplicationNames(fileName) ?: return
+    val application: Application = getApplication(teamApplicationNames) ?: return
     val competition = Competition(MetaInfo(name, date, sport), application)
     StartProtocol(competition.groupList, dir + competition.info.name + "/")
     competition.toCompetitionData().save(dir + competition.info.name + "/competitionData.csv")
@@ -97,63 +71,117 @@ fun start(inputData: Array<String>) {
 }
 
 fun finishByAthletes(inputData: Array<String>) {
-    if (inputData.size != FieldsFinish.values().size) {
-        println("Вы ввели что-то не то. Попробуйте еще раз.")
-        return
-    }
+    if (checkForEmptyInput(inputData)) return
     val fileName = inputData[FieldsFinish.PATH_TO_FILE.ordinal]
     val name = inputData[FieldsFinish.NAME.ordinal]
-    val data: List<List<String>>
-    val competition: Competition
-    if (!File(dir + name + "/competitionData.csv").exists()) {
-        println("Такого соревнования еще не проводилось. Сначала введите заявки на участие.")
-        return
-    }
-        data = csvReader().readAll(File(dir + name + "/competitionData.csv"))
-        competition = Competition(CompetitionData(data))
-    
-    val athletesResults: ResultData
-    try {
-        athletesResults =
-            ResultData(InputCompetitionResultByAthletes(fileName).toTable(), CompetitionData(data).getStartTime())
-    } catch (e: Exception) {
-        println(e.message)
-        return
-    }
+    if (checkCompetitionExist(name)) return
+    val data: List<List<String>> = getData(name) ?: return
+    val competition: Competition = getCompetition(data) ?: return
+    val athletesResults: ResultData = resultDataByAthlete(fileName, data) ?: return
     FinishProtocol(athletesResults, competition)
     println("Финальные протоколы для соревнования ${competition.info.name} сохранены в src/main/resources/competitions/${competition.info.name}/finishProtocol/.")
 }
 
 fun finishByCheckPoints(inputData: Array<String>) {
-    if (inputData.size != FieldsFinish.values().size) {
-        println("Вы ввели что-то не то. Попробуйте еще раз.")
-        return
-    }
+    if (checkForEmptyInput(inputData)) return
     val fileName = inputData[FieldsFinish.PATH_TO_FILE.ordinal]
-    val name = inputData[FieldsStart.NAME.ordinal]
-    val data: List<List<String>>
-    val competition: Competition
-    if (!File(dir + name + "/competitionData").exists()) {
-        println("Такого соревнования еще не проводилось. Сначала введите заявки на участие.")
-        return
-    }
+    val name = inputData[FieldsFinish.NAME.ordinal]
+    if (checkCompetitionExist(name)) return
+    val data: List<List<String>> = getData(name) ?: return
+    val competition: Competition = getCompetition(data) ?: return
+    val athletesResults: ResultData = resultDataByCheckPoints(fileName, data, competition) ?: return
+    FinishProtocol(athletesResults, competition)
+    println("Финальные протоколы для соревнования ${competition.info.name} сохранены в src/main/resources/competitions/${competition.info.name}/finishProtocol/.")
+}
+
+private fun getApplication(teamApplicationNames: List<String>): Application? {
     try {
-        data = csvReader().readAll(File(dir + name + "/competitionData.csv"))
-        competition = Competition(CompetitionData(data))
+        return Application(teamApplicationNames.map {
+            File(it)
+        })
     } catch (e: Exception) {
-        println("Произошла ошибка, попробуйте заново загрузить заявки.")
-        return
+        println(e.message)
+        return null
     }
-    val athletesResults: ResultData
+}
+
+private fun getTeamApplicationNames(fileName: String): List<String>? {
     try {
-        athletesResults = ResultData(
+        return File(fileName).readLines()
+    } catch (e: Exception) {
+        println("Файл $fileName не может быть прочитан.")
+        return null
+    }
+}
+
+private fun getDate(dateString: String): LocalDate? {
+    try {
+        return dateString.toLocalDate()
+    } catch (e: Exception) {
+        println("Дата состязания $dateString не корректна.")
+        return null
+    }
+}
+
+private fun checkSportType(inputData: Array<String>): Boolean {
+    if (sport == SportType.ERROR) {
+        println("Спорт ${inputData[FieldsStart.SPORT_TYPE.ordinal]} наша система не поддерживает.")
+        return true
+    }
+    return false
+}
+
+private fun checkForEmptyInput(inputData: Array<String>): Boolean {
+    if (inputData.size != FieldsStart.values().size) {
+        println("Вы ввели что-то не то. Попробуйте еще раз.")
+        return true
+    }
+    return false
+}
+
+private fun resultDataByAthlete(fileName: String, data: List<List<String>>): ResultData? {
+    try {
+        return ResultData(InputCompetitionResultByAthletes(fileName).toTable(), CompetitionData(data).getStartTime())
+    } catch (e: Exception) {
+        println(e.message)
+        return null
+    }
+}
+
+private fun resultDataByCheckPoints(fileName: String, data: List<List<String>>, competition: Competition): ResultData? {
+    try {
+        return ResultData(
             InputCompetitionResultByCheckPoints(fileName, competition).toTable(),
             CompetitionData(data).getStartTime()
         )
     } catch (e: Exception) {
         println(e.message)
-        return
+        return null
     }
-    FinishProtocol(athletesResults, competition)
-    println("Финальные протоколы для соревнования ${competition.info.name} сохранены в src/main/resources/competitions/${competition.info.name}/finishProtocol/.")
+}
+
+private fun getCompetition(data: List<List<String>>): Competition? {
+    try {
+        return Competition(CompetitionData(data))
+    } catch (e: Exception) {
+        println(e.message)
+        return null
+    }
+}
+
+private fun getData(name: String): List<List<String>>? {
+    try {
+        return csvReader().readAll(File(dir + name + "/competitionData.csv"))
+    } catch (e: Exception) {
+        println("Что-то пошло не так, попробуйте ввести заявки заново.")
+        return null
+    }
+}
+
+private fun checkCompetitionExist(name: String): Boolean {
+    if (!File(dir + name + "/competitionData.csv").exists()) {
+        println("Такого соревнования еще не проводилось. Сначала введите заявки на участие.")
+        return true
+    }
+    return false
 }
