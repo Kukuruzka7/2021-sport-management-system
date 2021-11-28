7235f6fadc56ad8bb407bdc08d58c27b5126089f
ksenono <danil.sibgatulllin@gmail.com>
2021-11-28 18:34:35 +0300

    Нас никуда не возьмут на работу

--- src/main/kotlin/ru/emkn/kotlin/sms/athlete/Category.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/athlete/Category.kt
@@ -7,15 +7,15 @@ enum class Category {
 
     companion object {
         private val categoryMap = setOf("", "нет", "никакой", "no", "none", "na").associateWith { NONE }.toMutableMap()
-            .apply { putAll(setOf("IIIю", "3ю", "3 юношеский", "third junior").associateWith { III_JUNIOR }) }
-            .apply { putAll(setOf("IIю", "2ю", "2 юношеский", "second junior").associateWith { II_JUNIOR }) }
-            .apply { putAll(setOf("Iю", "1ю", "1 юношеский", "first junior").associateWith { I_JUNIOR }) }
-            .apply { putAll(setOf("III", "3", "3 взрослый", "third").associateWith { III_JUNIOR }) }
-            .apply { putAll(setOf("II", "2", "2 взрослый", "second").associateWith { III_JUNIOR }) }
-            .apply { putAll(setOf("I", "1", "1 взрослый", "first").associateWith { III_JUNIOR }) }
-            .apply { putAll(setOf("КМС", "кандидат в мастера спорта", "candidate").associateWith { CANDIDATE }) }
-            .apply { putAll(setOf("МС", "мастера спорт", "master").associateWith { MASTER }) }
-            .apply { putAll(setOf("ММС", "международный мастер").associateWith { INTERNATIONAL_MASTER }) }.toMap()
+            .apply { putAll(setOf("iiiю", "3ю", "3 юношеский", "third junior").associateWith { III_JUNIOR }) }
+            .apply { putAll(setOf("iiю", "2ю", "2 юношеский", "second junior").associateWith { II_JUNIOR }) }
+            .apply { putAll(setOf("iю", "1ю", "1 юношеский", "first junior").associateWith { I_JUNIOR }) }
+            .apply { putAll(setOf("iii", "3", "3 взрослый", "third").associateWith { III_JUNIOR }) }
+            .apply { putAll(setOf("ii", "2", "2 взрослый", "second").associateWith { III_JUNIOR }) }
+            .apply { putAll(setOf("i", "1", "1 взрослый", "first").associateWith { III_JUNIOR }) }
+            .apply { putAll(setOf("кмс", "кандидат в мастера спорта", "candidate").associateWith { CANDIDATE }) }
+            .apply { putAll(setOf("мс", "мастера спорт", "master").associateWith { MASTER }) }
+            .apply { putAll(setOf("ммс", "международный мастер").associateWith { INTERNATIONAL_MASTER }) }.toMap()
 
 
         private val toRussian = mapOf(
@@ -24,12 +24,9 @@ enum class Category {
             CANDIDATE to "КМС", MASTER to "МС", INTERNATIONAL_MASTER to "ММС",
             X to "эксепшн кидать или это unexpected behaviour у toString будет? прив как дела"
         )
+
         //дает разряд по строке
-        fun getCategory(str: String): Category {
-            if (categoryMap.containsKey(str)) {
-                return categoryMap[str]!!
-            }
-            return X
-        }
+        fun getCategory(str: String): Category = categoryMap[str.lowercase()] ?: X
+
     }
 }
