754e04b56cf315e8da39bd32a01d1f972aa9cec7
TEAmofey <teamofey1982@gmail.com>
2021-12-12 00:43:15 +0300

    Что-то табличное

--- src/main/kotlin/ru/emkn/kotlin/sms/view/View.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/view/View.kt
@@ -1,17 +1,12 @@
 package ru.emkn.kotlin.sms.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -25,46 +20,61 @@ object View {
             Window(
                 onCloseRequest = ::exitApplication,
                 title = "Compose for Desktop",
-                state = rememberWindowState(width = 300.dp, height = 300.dp)
+                state = rememberWindowState(width = 1280.dp, height = 1080.dp)
             ) {
-                val count = remember { mutableStateOf(0) }
+                val firstRow = listOf("Фамилия", "Имя")
+                val rows = MutableList(0) { MutableList(0) { mutableStateOf("") } }
                 val names = listOf("Розалина", "Данил", "Тимофей")
-                var name = names.random()
+                val count = remember { mutableStateOf(rows.size) }
                 MaterialTheme {
-                    var consumedText by remember { mutableStateOf(0) }
-                    var text by remember { mutableStateOf("") }
                     Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                        TextField(
-                            value = text,
-                            onValueChange = { text = it },
+                        Row(Modifier.fillMaxWidth(), Arrangement.spacedBy(5.dp), Alignment.Top) {
+                            TextField(
+                                modifier = Modifier.height(50.dp).width(300.dp),
+                                singleLine = true,
+                                value = firstRow[0],
+                                onValueChange = { },
+                                readOnly = true,
                             )
-                        TextField(
-                            value = text,
-                            onValueChange = { text = it },
-
+                            TextField(
+                                modifier = Modifier.height(50.dp).width(300.dp),
+                                singleLine = true,
+                                value = firstRow[1],
+                                onValueChange = { },
+                                readOnly = true,
                             )
-
+                        }
+                        repeat(count.value) { i ->
+                            Row(Modifier.fillMaxWidth(), Arrangement.spacedBy(5.dp), Alignment.Top) {
+                                TextField(
+                                    modifier = Modifier.height(50.dp).width(300.dp),
+                                    singleLine = true,
+                                    value = rows[i][0].value,
+                                    onValueChange = { rows[i][0].value = it },
+                                )
+                                TextField(
+                                    modifier = Modifier.height(50.dp).width(300.dp),
+                                    singleLine = true,
+                                    value = rows[i][1].value,
+                                    onValueChange = { rows[i][1].value = it },
+                                )
+                            }
+                        }
                         Button(modifier = Modifier.align(Alignment.CenterHorizontally),
                             onClick = {
-                                consumedText++
-                                count.value++
-                                name = names.random()
+                                rows.add(listOf(mutableStateOf("") , mutableStateOf(names.random())).toMutableList())
+                                count.value = rows.size
+                                println(rows.size)
                             }) {
-                            Text(if (count.value == 0) "Hello World" else "${count.value}) $name")
+                            Text(if (rows.size == 0) "Hello World" else "${rows.size}")
                         }
                         Button(modifier = Modifier.align(Alignment.CenterHorizontally),
                             onClick = {
                                 count.value = 0
+                                rows.clear()
                             }) {
                             Text("Reset")
                         }
-                    }
-                    Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                        Text("Consumed text: $consumedText")
-                        TextField(
-                            value = text,
-                            onValueChange = { text = it },
-                            )
                     }
                 }
             }
